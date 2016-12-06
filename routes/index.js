a6bfdab7e8960ff59b1f1c10a0a4d47ead34f0d7
pmparmar <priyaparmar1794@gmail.com>
2016-12-06 14:41:48 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,92 @@
-const newRoutes = require("./api");
-const todoData = require('../data');
+const passport = require('passport'),
+	LocalStrategy = require('passport-local').Strategy,
+	path = require('path'),
+	users = require('../data/');
 
-const constructorMethod = (app) => {
-    app.use("/new", newRoutes);
+const loginRoutes = require("./login");
+const signupRoutes = require("./signup");
 
-    app.get("/", function (request, response) {
-    	// console.log('no such todo')
-        response.render("home", { todoItems: todoData.getAll()});
-    });
+let configurePassport = (passport) => {
 
-    app.use("*", (req, res) => {
-        res.sendStatus(404);
-    })
-};
+	// local strategy to verify username ans password
+	passport.use(new LocalStrategy(
+		(username, password, done) => {
 
-module.exports = constructorMethod;
+			let res = users.verifyUserPass(username, password);
+
+			if (res.status)
+				return done(null, res.message);
+
+			return done(null, false, {
+				message: res.message
+			});
+		}
+	));
+
+	passport.serializeUser((user, done) => {
+		done(null, user);
+	});
+
+	passport.deserializeUser((user, done) => {
+
+		let auth = user.split(' ');
+		if (auth.length != 2)
+			return done(null, false, {
+				message: "Cookie is not valid"
+			});
+
+		let username = auth[0];
+		let password = auth[1];
+
+		let res = users.verifyUserPass(username, password);
+
+		if (res.status)
+			return done(null, res.message);
+
+		return done(null, false, {
+			message: res.message
+		});
+	});
+}
+
+configurePassport(passport);
+
+
+let configureRoutes = (app) => {
+
+	app.use(passport.initialize());
+	app.use(passport.session());
+
+	app.use('/login',loginRoutes);
+
+	app.use('/signup',signupRoutes);
+	app.get('/private', (req, res) => {
+
+		if (req.isAuthenticated()) {
+			let username = req.user.split(' ')[0];
+			let userInfo = users.getUserById(username);
+			res.render('private.handlebars', {
+				username: username,
+				alias: userInfo.alias,
+				firstName: userInfo.firstName,
+				lastName: userInfo.lastName,
+				profession: userInfo.profession,
+				bio: userInfo.bio
+			});
+		} else {
+			res.redirect(301, '/login');
+		}
+
+	})
+
+	app.get('/', (req, res) => {
+		// protect endpoints
+		if (req.isAuthenticated()) {
+			res.redirect(301, '/private/');
+		} else {
+			res.redirect(301, '/login/')
+		}
+	})
+}
+
+module.exports = configureRoutes
