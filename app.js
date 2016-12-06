f7e83c99f6c68a46998672f10b36ea4849e6e13b
Alison1021 <yhunag48@stevens.edu>
2016-12-05 19:34:17 -0500

    init the route whole structure

--- app.js
+++ app.js
@@ -1,59 +1,83 @@
-// Siddharth Contractor #10402206
+const app = require("express")();
 
-const file = require("./fileData");
-const metric = require("./textMetrics");
+const static = express.static(__dirname + '/public');
 
-metric.createMetrics("Hello, my friends! This is a great day to say hello.")
-.catch(error => {
-    console.log(error);
+const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars');
+const handlebarsInstance = exphbs.create({
+    defaultLayout: 'main',
+    // Specify helpers which are only registered on this instance.
+    helpers: {
+        asJSON: (obj, spacing) => {
+            if (typeof spacing === "number")
+                return new Handlebars.SafeString(JSON.stringify(obj, null, spacing));
+
+            return new Handlebars.SafeString(JSON.stringify(obj));
+        }
+    },
+    partialsDir: [
+        'views/partials/'
+    ]
 });
 
-file.getFileAsString('chapter1.txt')
-.then(data => {
-	console.log('createMetrics on chapter1.txt')
-    console.log(typeof data)
-    
-	let obj = metric.createMetrics(data);
-    obj.then(data2 => {
-        console.log(data2);
-    }).catch(err => {
-        console.log(err)
+const configRoutes = require("./routes");
+
+const rewriteUnsupportedBrowserMethods = (req, res, next) => {
+    // If the user posts to the server with a property called _method, rewrite the request's method
+    // To be that method; so if they post _method=PUT you can now allow browsers to POST to a route that gets
+    // rewritten in this middleware to a PUT route
+    if (req.body && req.body._method) {
+        req.method = req.body._method;
+        delete req.body._method;
+    }
+
+    // let the next middleware run:
+    next();
+};
+
+
+// ---------- for debug only ----------
+app.use(function(req, res, next) {
+        console.log('req.method and url  is ' + req.method + " " + req.url);
+        console.log("req--------------------:")
+        console.log(req)
+        console.log('');
+        next();
     })
-})
-.catch(err => {
-	console.log(err);
-})
 
 
-file.getFileAsString('chapter2.txt')
-.then(data => {
-	console.log('createMetrics on chapter2.txt')
-    console.log(typeof data)
-    
-	let obj = metric.createMetrics(data);
-    obj.then(data2 => {
-        console.log(data2);
-    }).catch(err => {
-        console.log(err)
-    })
-})
-.catch(err => {
-	console.log(err);
-})
+
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false // true for https
+    }
+}));
+
+app.use(require('connect-flash')());
+app.use(require('cookie-parser')());
 
 
-file.getFileAsString('chapter3.txt')
-.then(data => {
-	console.log('createMetrics on chapter3.txt')
-    console.log(typeof data)
-    
-	let obj = metric.createMetrics(data);
-    obj.then(data2 => {
-        console.log(data2);
-    }).catch(err => {
-        console.log(err)
-    })
-})
-.catch(err => {
-	console.log(err);
-})
+app.use("/public", static);
+
+app.use(require("body-parser").json());
+app.use(require("body-parser").urlencoded({
+    extended: true
+}));
+
+app.use(rewriteUnsupportedBrowserMethods);
+
+app.engine('handlebars', handlebarsInstance.engine);
+app.set('view engine', 'handlebars');
+
+
+
+
+configRoutes(app);
+
+app.listen(3000, () => {
+    console.log("We've now got a server!");
+    console.log("Your routes will be running on http://localhost:3000");
+});
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,213 @@
-const newRoutes = require("./api");
-const todoData = require('../data');
+//expecting from database
+const Data = require('../data');
+
+//expecting from login
+const configPassport = require('./passportconfig');
+
+const passport = require('passport');
+configPassport(passport);
+
 
 const constructorMethod = (app) => {
-    app.use("/new", newRoutes);
+    app.post('/login',
+        passport.authenticate('local', {
+            successRedirect: '/profile',
+            failureRedirect: '/login',
+            failureFlash: true
+        })
+    );
 
-    app.get("/", function (request, response) {
-    	// console.log('no such todo')
-        response.render("home", { todoItems: todoData.getAll()});
+    app.post('/signup', (req, res) => {
+        Data.user.Signup(req.body.email, req.body.password).then(info => {
+            console.log(info); //e.g.  sign up successed, usrname: pasword:
+
+            /* refer from
+            http://code.runnable.com/VKHrGJKvwo55gHL7/express-passport-js-login-and-register-for-node-js-and-hello-world            
+            */
+            passport.authenticate('local')(req, res, function() {
+                res.redirect('/profile');
+            });
+
+        }).catch(err => {
+            res.render('partials/sigup', {
+                email: req.body.email,
+                message: err
+            });
+        });
+
     });
+
+    app.get('/login', (req, res) => {
+        if (req.isAuthenticated()) {
+            res.redirect('/profile');
+        } else {
+            // message expecting from login
+            render('partials/login', {
+                message: req.flash('loginmessage')
+            });
+        }
+    });
+
+    app.get('/logout',
+        function(req, res) {
+            req.logout();
+            res.redirect('/');
+        });
+
+
+
+    app.get('/signup', (req, res) => {
+        render('partials/login', {
+            // message expecting from login
+            message: req.flash('signupmessage')
+        });
+
+    });
+
+    app.get('/profile',
+        require('connect-ensure-login').ensureLoggedIn(),
+        function(req, res) {
+            res.render('partials/profile', {
+                user: req.user
+            })
+        });
+
+    app.get('/', (req, res) => {
+
+        // expecting from database
+        // get user setting and prefer 
+        // in order to display the homepage postlist
+        // it will always work even user is undefined
+        Data.DefaultSearch(req.user).then(postlist => {
+            res.render('partials/home', {
+                list: postlist
+                user: req.user
+            });
+        }).catch(err => {
+            res.render('partials/home', {
+                user: req.user,
+                listerrormessage: err
+            })
+        });
+    });
+
+    app.get('/searchpost', (req, res) => {
+        //excepting searching ULR like :
+        // :3000/search?q=java
+        // then i got java
+        Data.post.Search(req.query.q).then(postlist => {
+            res.render('partials/home', {
+                list: postlist,
+                user: req.user
+            })
+        }).catch(err => {
+            res.render('partials/home', {
+                user: req.user,
+                listerrormessage: err
+            })
+        });
+    });
+
+    // search user ???  not sure
+    app.get('/searchuser', (req, res) => {
+        //excepting searching ULR like :
+        // :3000/search?q=java
+        // then i got java
+        Data.user.Search(req.query.q).then(userlist => {
+            res.render('partials/userlist', {
+                list: userlist,
+                user: req.user //logged in user
+            })
+        }).catch(err => {
+            res.render('partials/home', {
+                user: req.user,
+                listerrormessage: err
+            })
+        });
+    });
+
+    //the single post page
+    app.get('/post/:id', (req, res) => {
+        Data.findpostbyid(req.param.id).then(post => {
+            res.render('partials/singlepost', {
+                post: post
+            });
+        }).catch(err => {
+            res.render('partials/singlepost', {
+                message: err
+            });
+        });
+    });
+
+    //form page for creating a new post(quesiton)
+    app.get('/posting', (req, res) => {
+        res.render('partials/postform')
+    });
+
+    //post method to create a new post(quesiton)
+    app.post('/posting', (req.res) => {
+        if (!req.isAuthenticated()) {
+            res.render('partials/postform', {
+                loginerrormessage: 'Please login'
+            });
+        } else {
+            Data.posts.create(req.user, req.body).then(newpost => {
+                res.redirect('/post/{{newpost._id}}');
+            }).catch(err => {
+                res.render('partials/postform', {
+                    message: err
+                });
+            });
+        }
+    });
+
+    //post method to create a new anster, 
+    //no single page for answer, the form is on the bottom 
+    app.post('/answering', (req.res) => {
+        if (!req.isAuthenticated()) {
+            res.render('partials/postform', {
+                loginerrormessage: 'Please login'
+            });
+        } else {
+            Data.answers.create(req.user, req.body).then(locationhash => {
+
+                // answer successed, go to the new answer location of the same post page
+                res.redirect('/post/' + req.body.postid + '#' + locationhash);
+            }).catch(err => {
+
+                // answer falied, stay on the post page
+                res.redirect('/post/' + req.body.postid);
+
+            });
+        }
+    });
+
+    //post method to create a new comment, 
+    //no single page for comment, the form is inside the postpage 
+    app.post('/commenting', (req.res) => {
+        if (!req.isAuthenticated()) {
+
+            res.render('partials/postform', {
+                loginerrormessage: 'Please login'
+            });
+        } else {
+            Data.cmoments.create(req.user, req.body).then(locationhash => {
+
+                // comment successed, go to the new answer comment of the same post page
+                res.redirect('/post/' + req.body.postid + '#' + locationhash);
+            }).catch(err => {
+
+                // comment falied, stay on the post page
+                res.redirect('/post/' + req.body.postid);
+
+            });
+        }
+    });
+
 
     app.use("*", (req, res) => {
         res.sendStatus(404);
-    })
+    });
 };
 
 module.exports = constructorMethod;
