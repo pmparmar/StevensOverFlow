54d0f136c2c048ae62ae9134c8789fab1e1b9aa6
pmparmar <priyaparmar1794@gmail.com>
2016-12-06 14:46:11 -0500

    Update index.js

--- data/index.js
+++ data/index.js
@@ -25,7 +25,7 @@ let users = {
 		firstName: "Harry",
 		lastName: "Potter",
 		profession: "Student",
-		bio: "Harry Potter is a series of fantasy novels written by British author J. K. Rowling. The novels chronicle the life of a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry . The main story arc concerns Harry's struggle against Lord Voldemort, a dark wizard who intends to become immortal, overthrow the wizard governing body known as the Ministry of Magic, and subjugate all wizards
+		bio: "Harry Potter is a series of fantasy novels written by British author J. K. Rowling. The novels chronicle the life of a young wizard, Harry Potter, and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry . The main story arc concerns Harry's struggle against Lord Voldemort, a dark wizard who intends to become immortal, overthrow the wizard governing body known as the Ministry of Magic, and subjugate all wizards"
 	}
 }
 
